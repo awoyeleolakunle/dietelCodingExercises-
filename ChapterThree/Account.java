be9b77fee303a04e608108b09ac97adb81e425d4
Awoyele Olakunle <awoyeleolakunle@gmail.com>
2022-11-10 17:13:59 -0800

    initial commit

---
+++ ChapterThree/Account.java
@@ -0,0 +1,33 @@
+public class Account{
+
+	private String name;
+	private double balance;
+
+
+
+	public Account(){
+
+
+}
+
+	public Account (String name, double balance){
+
+		this.name=name;
+		this.balance=balance;
+}
+
+	public void setName(String name){
+		this.balance=balance;
+}
+
+
+	public String getName(){
+		return name;
+}
+
+
+	public double getBalance(){
+		return balance;
+}
+
+}
