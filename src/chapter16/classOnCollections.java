e1b9b99abfa0b97ef1d8f37a6f356b581a20fcfe
Awoyele Olakunle <awoyeleolakunle@gmail.com>
2023-04-12 13:42:10 -0700

    new commit

---
+++ src/chapter16/classOnCollections.java
@@ -0,0 +1,14 @@
+package chapter16;
+
+import java.util.List;
+
+public class classOnCollections {
+    public static void main(String[] args) {
+        List <String> names = List.of("Prof", "Kabir", "Fm");
+        System.out.println(names.get(0));
+        System.out.println(names.get(1));
+        System.out.println(names.get(2));
+    }
+
+
+}
